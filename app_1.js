d76c7caafa658e566af9ac5650f32104bfbe57cf
akswwebsoft <info@aksindia.com>
2021-01-29 09:23:19 +0530

    first app upload

---
+++ app_1.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+
+const App = () => {
+  const Stack = createStackNavigator();
+  return (
+    
+            <NavigationContainer>
+              <Stack.Navigator initialRouteName="Details">
+              <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+              <Stack.Screen name="Detials" component={DetailsScreen}></Stack.Screen>
+              </Stack.Navigator>
+              </NavigationContainer>
+    
+  );
+};
+
+function HomeScreen(){
+  return(
+    <View>
+      <Text>
+        Home Screen
+      </Text>
+      <Button title="go to Details"></Button>
+    </View>
+  )
+}
+
+function DetailsScreen(){
+  return(
+    <View>
+      <Text>
+      Details Screen
+      </Text>
+    </View>
+  )
+}
+
+export default App;
